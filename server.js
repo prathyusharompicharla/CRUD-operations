7d8ae558cfd2f4b842d7428f392f637071d54360
prathyusha <rprathyusha28@gmail.com>
2021-01-19 11:37:19 +0530

    loginApi

---
+++ server.js
@@ -0,0 +1,102 @@
+var express = require("express");
+
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const cors = require("cors");
+
+
+const router = express.Router();
+app.use("/", router);
+
+const User=require('./Models/User')
+const Company=require('./Models/Company')
+const Exceljs=require('exceljs')
+var connect=require("./db");
+
+
+var { buildSchema } = require('graphql');
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+// Root resolver
+var root = {
+    message: () => 'Hello World!'
+};
+// Create an express server and a GraphQL endpoint
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+
+
+app.use('/',require('./Controller/pincode_controller'))
+app.use("/users", require("./Routes/user_route"));
+
+
+app.use('*', cors());
+
+
+
+
+app.get('/sheet',(req,res)=>{
+  
+      User.find({},(err,users)=>{
+        console.log(users)
+        let workbook = new Exceljs.Workbook(); 
+        let worksheet = workbook.addWorksheet('My User');
+        worksheet.columns = [
+          { header: 'Id', key: '_id', width: 10 },
+          { header: 'Name', key: 'firstName', width: 30 },
+          { header: 'Name', key: 'lastName', width: 30 },
+          { header: 'email', key: 'email', width: 30},
+          { header: 'phoneNumber', key: 'tel', width: 30},
+          { header: 'adress', key: 'adress', width: 30},
+          { header: 'company', key: 'company', width: 30},
+        ];
+        console.log(users)
+        users.forEach(user => {
+          worksheet.addRow(user)
+        });
+  
+      const data=  workbook.xlsx.writeFile("customer.xlsx")
+            console.log(data)
+              console.log("file saved!");
+            res.send('done')
+         
+      })
+    
+//creating workbook
+	 //creating worksheet
+    
+  })
+
+  app.use(() => (req, res, next)  =>{
+    res.locals.user = req.user; // This is the important line
+    // req.session.user = user
+    console.log(res.locals.user);
+    next();
+  });
+
+
+var server=app.listen(5000);
+console.log("Listening to PORT 5000");
+
+module.exports=server;
